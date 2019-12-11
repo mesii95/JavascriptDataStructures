be1eae4a8752d03368ed2d6c43e5c411e9476ee8
Michael Schroeder <95.michael.schroeder@gmail.com>
2019-12-11 11:43:59 -0600

    Bug in add with index

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -4,35 +4,60 @@ class LinkedList {
         this.head = null;
         this.tail = null;
         this.size = 0;
-    }
-
-    add(data, index){
-        if(typeof index == 'number'){
-            if(this.size === 0){
+        this._insertLastIndex = function(data){
+                let n = this.get(this.size-1);
+                let newNode = new Node(n, n.previous, data);
+                n.previous.setNext(newNode);
+                n.setPrev() = newNode;
+                this.size++;
+            };
+        this._insertFirstIndex = function(data){
+                let n = this.get(0);
+                let newNode = new Node(n, null, data);
+                n.setPrev(newNode);
+                this.head = newNode;
+                this.size++;
+            };
+        this._insert = function(data, index){
+                let n1 = this.get(index);
+                let n2 = n1.previous;
+                let newNode = new Node(n1, n2, data);
+                n1.setPrev(newNode);
+                n2.setNext(newNode);
+            };
+        this._addToEmpty = function(data){
                 this.head = new Node(null, null, data);
                 this.tail = this.head;
                 this.size++;
-            }else{
+            };
+        this._addAfterTail = function(data){
                 let n = new Node(null, this.tail, data);
                 this.tail.setNext(n);
                 this.tail = this.tail.next;
                 this.size++;
+            };
+        }
+
+    add(data, index){
+        if(index == null){
+            if(this.size === 0){
+                this._addToEmpty(data);
+            }else{
+                this._addAfterTail(data);
             }
         }else {
             if(index >= this.size) { return null; }
-            
-            if(index == this.size -1){
-                n = this.get(index);
-                newNode = Node(n, n.previous, data);
-    
-                n.previous.setNext() = newNode;
-                n.setPrev() = newNode;
+            if(this.isEmpty === true) { this._addToEmpty(data) };
+            if(this.size === 1 && index === 0) { this._insertFirstIndex(data); }
+
+            if(index == this.size-1){
+                this._insertLastIndex(data);
             }
             else if(index == 0){
-                // TODO
+                this._insertFirstIndex(data);
             }
             else {
-                // TODO
+                this._insert(data, index);
             }
         }
         return(data);
@@ -59,6 +84,10 @@ class LinkedList {
         return removed;
     }
 
+    isEmpty(){
+        return this.size == 0;
+    }
+
     getTail(){
         return this.tail;
     }
@@ -69,10 +98,10 @@ class LinkedList {
     
     get(index){
         if(typeof index != 'number'){ return null; }
-        let n = null;
+        let n = this.head;
 
-        for(let i=0; i<=index; i++){
-            n = this.data.next;
+        for(let i=0; i<index; i++){
+            n = this.next;
         }
         
         return n;
